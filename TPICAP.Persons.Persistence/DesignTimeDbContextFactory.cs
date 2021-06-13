bb82170bcf391c7babea1bef488ba1e29bab9453
Cl0v15 <80980810+Cl0v15@users.noreply.github.com>
2021-06-13 18:49:53 +0100

    feat: docker-compose

---
+++ TPICAP.Persons.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TPICAP.Persons.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../TPICAP.Persons.API"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+
+            builder.UseSqlServer(configuration.GetConnectionString("AppEntities"));
+
+            return new AppDbContext(builder.Options, configuration);
+        }
+    }
+}
